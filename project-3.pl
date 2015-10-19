474a4ee1ab2a761c01727499304234fb74eb31ab
rogersmatt <rogers.md.s@141H13.petoskeyschools.org>
2015-10-19 14:57:04 -0400

    project-3

---
+++ project-3.pl
@@ -0,0 +1,13 @@
+# Assingment: Project 3
+# Author: Matt Rogers
+# Verzion: 10.12.2015
+
+use 5.14.1;
+use warnings;
+
+my ($zipCode, $age, $numItemsOrdered);
+use constant MIN_ZIP => 00501;
+use constant MAX_ZIP => 99950;
+use constant MIN_AGE => 10;
+use constant MAX_AGE => 110;
+
